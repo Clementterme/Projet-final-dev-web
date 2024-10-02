af1dcde65f16b4d541e6932e74f1b2a100a3ac71
Clément <clement.terme.simplon@gmail.com>
2024-10-02 12:30:27 +0200

    Création architecture

---
+++ src/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'trouvanime');
+define('DB_USER', 'trouvanime');
+define('DB_PWD', '1234');
+
+define('HOME_URL', '/');
+
+define('DB_INITIALIZED', TRUE);
