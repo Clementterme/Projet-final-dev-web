4ee023ef43919e4702fff2e50338c73bfad916f7
Clément <clement.terme.simplon@gmail.com>
2024-10-21 19:59:56 +0200

    Gestion des erreurs connexion/inscription

--- src/Controllers/ConnexionController.php
+++ src/Controllers/ConnexionController.php
@@ -20,31 +20,33 @@ class ConnexionController {
             if (!empty($_POST["email"]) && !empty($_POST["mdp"])) {
                 $email = htmlspecialchars($_POST['email']);
                 $mdp = htmlspecialchars($_POST['mdp']);
-                // $mdpsafe = password_verify($mdp);
 
-                $selectUser = $bdd->prepare("SELECT * FROM utilisateur WHERE email = ? AND mdp = ?");
-                $selectUser->execute(array($email, $mdp));
+                $selectUser = $bdd->prepare("SELECT * FROM utilisateur WHERE email = ?");
+                $selectUser->execute(array($email));
 
                 if ($selectUser->rowCount() > 0) {
-                    $_SESSION["email"] = $email;
-                    $_SESSION["mdp"] = $mdp;
 
                     // Récupère les données de l'utilisateur qui se connecte pour les stocker dans la variable $_SESSION
                     $utilisateur = $selectUser->fetch();
 
-                    $_SESSION["role"] = $utilisateur["id_1"];
-                    $_SESSION["id"] = $utilisateur["id"];
+                    if (password_verify($mdp, $utilisateur["mdp"])) {
+                       
+                        $_SESSION["role"] = $utilisateur["id_1"];
+                        $_SESSION["id"] = $utilisateur["id"];
 
-                    $_SESSION['connecté'] = TRUE;
+                        $_SESSION['connecté'] = TRUE;
 
-                    if ($_SESSION["role"] == 0) {
-                        $_SESSION["role"] = "Utilisateur";
-                        header("location: " . HOME_URL);
-                        exit();
-                    } else if ($_SESSION["role"] == 1) {
-                        $_SESSION["role"] = "Administrateur";
-                        header("location: " . HOME_URL);
-                        exit();
+                        if ($_SESSION["role"] == 0) {
+                            $_SESSION["role"] = "Utilisateur";
+                            header("location: " . HOME_URL);
+                            exit();
+                        } else if ($_SESSION["role"] == 1) {
+                            $_SESSION["role"] = "Administrateur";
+                            header("location: " . HOME_URL);
+                            exit();
+                        }
+                    } else {
+                        echo "Email ou mot de passe incorrect";
                     }
                 } else {
                     echo "Email ou mot de passe incorrect";
@@ -65,18 +67,27 @@ class ConnexionController {
             if (!empty($_POST["email"]) && !empty($_POST["mdp"]) && !empty($_POST["mdp2"])) {
                 if ($_POST["mdp"] == ($_POST["mdp2"])) {
                     $email = htmlspecialchars($_POST['email']);
-                    // $mdp = htmlspecialchars($_POST['mdp']);
                     $mdp = password_hash($_POST['mdp'], PASSWORD_DEFAULT);
+
+
+                    // Vérifie si l'email est déjà utilisé
+                    $checkUser = $bdd->prepare("SELECT * FROM utilisateur WHERE email = ?");
+                    $checkUser->execute(array($email));
+
+                    if ($checkUser->rowCount() > 0) {
+                        echo "Cet email est deja utilisé.";
+                        $this->render("inscription");
+                    }
+
+
                     $insertUser = $bdd->prepare("INSERT INTO utilisateur(email, mdp) VALUES(?, ?)");
                     $insertUser->execute(array($email, $mdp));
 
 
                     // Récupérer l'utilisateur pour le connecter après son inscription
-                    $selectUser = $bdd->prepare("SELECT * FROM utilisateur WHERE email = ? AND mdp = ?");
-                    $selectUser->execute(array($email, $mdp));
+                    $selectUser = $bdd->prepare("SELECT * FROM utilisateur WHERE email = ?");
+                    $selectUser->execute(array($email));
                     if ($selectUser->rowCount() > 0) {
-                        $_SESSION["email"] = $email;
-                        $_SESSION["mdp"] = $mdp;
 
                         $utilisateur = $selectUser->fetch();
 
